e65b8c98ecaffdfb142806ac79bcd688b81f5479
Claudiu Popa <popa.clctin@gmail.com>
2022-08-21 17:30:15 +0300

    Add required validation to user and post paths in Like schema

--- backend/models/Like.js
+++ backend/models/Like.js
@@ -6,10 +6,12 @@ const likeSchema = new Schema(
     user: {
       type: Schema.Types.ObjectId,
       ref: 'User',
+      required: [true, 'User is required'],
     },
     post: {
       type: Schema.Types.ObjectId,
       ref: 'Post',
+      required: [true, 'Post is required'],
     },
   },
   {
