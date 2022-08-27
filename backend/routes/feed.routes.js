c4b830b79693d1053effd1b690ffae2ee6d99ab8
Claudiu Popa <popa.clctin@gmail.com>
2022-08-27 09:42:54 +0300

    Add validation to the feed routes

--- backend/routes/feed.routes.js
+++ backend/routes/feed.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { body, param } = require('express-validator');
+const { sanitizeId } = require('../utils/sanitizers');
 
 const feedController = require('../controllers/feed.controller');
 
-router.get('/:userId', feedController.getFeed);
+router.get(
+  '/:userId',
+  param('userId').notEmpty().customSanitizer(sanitizeId),
+  feedController.getFeed
+);
 
 module.exports = router;
