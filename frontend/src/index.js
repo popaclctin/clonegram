1a64597264a2cc96ad3e9a9d0e2793cfab9d0088
Claudiu Popa <popa.clctin@gmail.com>
2022-08-31 14:07:56 +0300

    Install react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Hello from './Hello';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<Hello />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
